d2bc62e0a467a2f2290cccf0b1526ab2190b893e
Tom <rinuo@qq.com>
2026-09-03 20:09:17 +0800

    突破6模块上限：合并所有模块到 modules/index.js，首页只需一次 require

---
+++ src/pages/index/modules/index.js
@@ -0,0 +1,13 @@
+var modules = {}
+
+modules.clock = require("./clock.js")
+modules.dayNav = require("./day-nav.js")
+modules.statusBar = require("./status-bar.js")
+modules.customContent = require("./custom-content.js")
+modules.pinnedPages = require("./pinned-pages.js")
+modules.classList = require("./class-list.js")
+modules.quickAdd = require("./quick-add.js")
+modules.bottomButtons = require("./bottom-buttons.js")
+modules.weekIndicator = require("./week-indicator.js")
+
+module.exports = modules
