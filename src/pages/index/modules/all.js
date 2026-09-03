b4e97777e6596b79fd44010b829dca28f8d1b2a8
Tom <rinuo@qq.com>
2026-09-03 10:50:44 +0800

    refactor: 合并为单模块 all.js(utils+clock), 只用一个require

--- src/pages/index/modules/all.js
+++ src/pages/index/modules/all.js
@@ -1,3 +1,22 @@
+var dayNames = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+
+function parseTime(timeStr) {
+  var parts = timeStr.split(":")
+  var hours = parseInt(parts[0]) || 0
+  var minutes = parseInt(parts[1]) || 0
+  return hours * 60 + minutes
+}
+
+function getRealTodayIndex() {
+  return new Date().getDay()
+}
+
+function formatTime(minutes) {
+  var h = Math.floor(minutes / 60)
+  var m = minutes % 60
+  return (h < 10 ? "0" : "") + h + ":" + (m < 10 ? "0" : "") + m
+}
+
 function updateClock(instance) {
   var now = new Date()
   var tf = instance.timeFormat || { year: false, month: false, day: false, hour: true, minute: true, second: false }
@@ -19,5 +38,9 @@ function updateClock(instance) {
 }
 
 export default {
+  dayNames: dayNames,
+  parseTime: parseTime,
+  getRealTodayIndex: getRealTodayIndex,
+  formatTime: formatTime,
   updateClock: updateClock
 }
