7d02bae4c9e8811506f22a5965d5ea9e3368bced
Tom <rinuo@qq.com>
2026-09-03 21:55:02 +0800

    修复：根因是每个模块最多5个instance方法赋值。将quick-add方法打包到quickAdd对象内(仅1个赋值)，突破限制

--- src/data/version.js
+++ src/data/version.js
@@ -1,1 +1,1 @@
-module.exports = { versionName: "1.3.277", versionCode: 407 }
+module.exports = { versionName: "1.3.282", versionCode: 412 }
--- src/pages/index/modules/clock.js
+++ src/pages/index/modules/clock.js
@@ -35,18 +35,74 @@ function init(instance) {
 
   instance.startClockTimer()
 
-  instance.quickAddExpanded = false
-  instance.quickCourseNames = []
-  instance.quickAddDisabled = false
-  instance.quickAddTime = ""
-
-  instance.toggleQuickAdd = function() {
-    var self = instance
-    self.quickAddExpanded = !self.quickAddExpanded
-  }
-
-  instance.loadPresetCourses = function() {
-    console.log("loadPresetCourses defined")
+  instance.quickAdd = {
+    expanded: false,
+    courseNames: [],
+    disabled: false,
+    time: "",
+    toggle: function() {
+      var self = instance
+      self.quickAdd.expanded = !self.quickAdd.expanded
+      if (self.quickAdd.expanded) {
+        self.quickAdd.loadPreset()
+        self.quickAdd.time = self.quickAdd.calcNext()
+      }
+    },
+    loadPreset: function() {
+      var self = instance
+      var storage = require("@system.storage")
+      storage.get({
+        key: "course_preset_list",
+        success: function(data) {
+          try {
+            var list = JSON.parse(data)
+            var names = []
+            for (var i = 0; i < list.length; i++) {
+              var name = list[i].name || list[i]
+              names.push(name)
+            }
+            self.quickAdd.courseNames = names
+          } catch (e) {
+            self.quickAdd.courseNames = []
+          }
+        },
+        fail: function() {
+          self.quickAdd.courseNames = []
+        }
+      })
+    },
+    calcNext: function() {
+      var self = instance
+      var now = new Date()
+      var nowMinutes = now.getHours() * 60 + now.getMinutes()
+      var lastEndMin = 0
+      var classes = self.currentClasses || []
+      for (var i = 0; i < classes.length; i++) {
+        var parts = classes[i].time.split("-")
+        if (parts.length < 2) continue
+        var endMin = self.quickAdd._parseTime(parts[1].trim())
+        if (endMin > lastEndMin) lastEndMin = endMin
+      }
+      var startMin = lastEndMin > 0 ? lastEndMin + 10 : 8 * 60
+      if (startMin < nowMinutes) startMin = nowMinutes + 5
+      if (startMin >= 24 * 60) {
+        self.quickAdd.disabled = true
+        return "已排满"
+      }
+      var endMin = startMin + 45
+      if (endMin > 24 * 60) endMin = 24 * 60
+      self.quickAdd.disabled = false
+      return self.quickAdd._fmt(startMin) + " - " + self.quickAdd._fmt(endMin)
+    },
+    _fmt: function(minutes) {
+      var h = Math.floor(minutes / 60)
+      var m = minutes % 60
+      return (h < 10 ? "0" : "") + h + ":" + (m < 10 ? "0" : "") + m
+    },
+    _parseTime: function(timeStr) {
+      var parts = timeStr.split(":")
+      return parseInt(parts[0]) * 60 + parseInt(parts[1])
+    }
   }
 }
 
